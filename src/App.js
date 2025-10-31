b6a34975faf049a5cac3d1ca834426c3165b2daa
ysagar558 <ysagar558@gmail.com>
2025-10-31 18:05:28 +0530

    First custom component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+
+
+
 
 
 function App() {
   return (
     <div>
       <h1>Let's get started</h1>
+      <ExpenseItem></ExpenseItem>
+      
     </div>
   );
 }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,10 @@
+function ExpenseItem(){
+    return (
+        <div>
+            <h1>Food - $5</h1>
+            <h1>Petrol - $10</h1>
+            <h1>Movie - $20</h1>
+        </div>
+    )
+}
+export default ExpenseItem;
