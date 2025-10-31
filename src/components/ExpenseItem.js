300ee0b9cb8307a2c1604663ab2e244d3341ecfe
ysagar558 <ysagar558@gmail.com>
2025-10-31 18:54:38 +0530

    component with CSS

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,15 @@
+import "./ExpenseItem.css"
+
 function ExpenseItem(){
     return (
-        <div>
-            <div>15 August 2023</div>
-            <div>
-                <h1>Book</h1>
-            </div>
-            <div>$10</div>
-            <div>Delhi</div>
-        </div>
+        <div className="expense-item">
+      <div>15 August 2023</div>
+      <div className="expense-item__description">
+        <h2>Book</h2>
+        <div className="expense-item__price">$10</div>
+        <div className="expense-item__location">Delhi</div>
+      </div>
+    </div>
     )
 }
 export default ExpenseItem;
