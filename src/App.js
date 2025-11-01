a427788f3cb01e826b644efde2b7c79efc0894f9
ysagar558 <ysagar558@gmail.com>
2025-11-01 15:14:04 +0530

    Reusable components using Map

--- src/App.js
+++ src/App.js
@@ -6,33 +6,29 @@ import ExpenseItem from "./components/ExpenseItem";
 
 
 function App() {
+const expenses=[
+  {id:"1",date:new Date(2023,7,27),title:"Insurance",price:100,location:"Bangalore"},
+  { id: "2", date: new Date(2023, 3, 25), title: "Book", price: 10,location:"Delhi" },
+    { id: "3", date: new Date(2023, 10, 11), title: "Pen", price: 1,location:"Hyderabad" },
+    { id: "4", date: new Date(2023, 1, 14), title: "Laptop", price: 200,location:"Mumbai" },
+]
+
   return (
     <div>
       <h1>Let's get started</h1>
-      <ExpenseItem
-        date={new Date(2023,7,27)}
-        title="Insurance"
-        price="20"
-        location="Bangalore"
-      ></ExpenseItem>
-      <ExpenseItem
-        date={new Date(2023, 3, 25)}
-        title="Book"
-        price="20"
-        location="Delhi"
-      ></ExpenseItem>
-      <ExpenseItem
-        date={new Date(2023, 10, 11)}
-        title="Pen"
-        price="5"
-        location="Hyderabad"
-      ></ExpenseItem>
-      <ExpenseItem
-        date={new Date(2023, 1, 14)}
-        title="Laptop"
-        price="200"
-        location="Mumbai"
-      ></ExpenseItem>
+      {
+        expenses.map((expense,index)=>{
+          return(
+            <ExpenseItem
+            key={expense.id}
+            date={expense.date}
+            title={expense.title}
+            price={expense.price}
+            location={expense.location}
+            ></ExpenseItem>
+          );
+        })
+      }
       
     </div>
   );
