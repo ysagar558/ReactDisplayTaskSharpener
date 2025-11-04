c24412f6d0dfa58914f0af103836a4cc08f8d766
ysagar558 <ysagar558@gmail.com>
2025-11-04 15:54:55 +0530

    Introduction to hooks

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,3 +1,4 @@
+import React ,{useState} from "react";
 import "./ExpenseItem.css"
 import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
@@ -7,15 +8,18 @@ function ExpenseItem(props){
   // const expenseTitle = "props.title";
   // const expenseAmount = 50;
   // const expenseLocation = "Bangalore";
+  const [title,setTitle]=useState(props.title);
+
+
   function buttonClickHandeler(event){
-    console.log("Button clicked");
+    setTitle("New Title");
   }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.price}</div>
       </div>
       <button onClick={buttonClickHandeler}>Change Title</button>
