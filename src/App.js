4498457d3870cdc651b7243fefb1e2b40367e041
ysagar558 <ysagar558@gmail.com>
2025-11-06 18:41:08 +0530

    Rendering expenses to list

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
+import React,{useState} from "react";
 import ExpenseItem from "./components/ExpenseItem";
 import Expenses from "./components/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
 
 function App() {
-const expenses=[
+const [expenses,setExpenses]= useState([
   {id:"1",date:new Date(2023,7,27),title:"Insurance",price:100,location:"Bangalore"},
   { id: "2", date: new Date(2023, 3, 25), title: "Book", price: 10,location:"Delhi" },
     { id: "3", date: new Date(2023, 10, 11), title: "Pen", price: 1,location:"Hyderabad" },
     { id: "4", date: new Date(2023, 1, 14), title: "Laptop", price: 200,location:"Mumbai" },
-]
+]);
 const addExpenseHandler = (expense) => {
-    console.log(expense);
+    setExpenses((prevExpenses)=>{
+      return[expense,...prevExpenses];
+    });
   };
 
 
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -21,7 +21,7 @@ const ExpenseForm = (props) => {
     event.preventDefault();
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      price: enteredAmount,
       date: new Date(enteredDate),
     }
     props.onSaveExpenseData(expenseData);
